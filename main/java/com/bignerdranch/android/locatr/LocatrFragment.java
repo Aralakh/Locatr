eea27b1b1823fab4d077b4a8377d4a1fda9d34df
Aralakh <servantofmanos@gmail.com>
2017-11-22 15:19:47 -0500

    adding googleplay connections

--- main/java/com/bignerdranch/android/locatr/LocatrFragment.java
+++ main/java/com/bignerdranch/android/locatr/LocatrFragment.java
@@ -1,13 +1,18 @@
 package com.bignerdranch.android.locatr;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 /**
  * Created by lawren on 22/11/17.
@@ -15,6 +20,7 @@ import android.widget.ImageView;
 
 public class LocatrFragment extends Fragment {
     private ImageView mImageView;
+    private GoogleApiClient mClient;
 
     public static LocatrFragment newInstance(){
         return new LocatrFragment();
@@ -24,13 +30,27 @@ public class LocatrFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+        mClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(@Nullable Bundle bundle) {
+                        getActivity().invalidateOptionsMenu();
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+
+                    }
+                })
+                .build();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_locatr, container, false);
 
-        mImageView = (ImageView) v.findViewById(R.id.image);
+        mImageView = v.findViewById(R.id.image);
         return v;
     }
 
@@ -38,5 +58,21 @@ public class LocatrFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_locatr, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_locate);
+        searchItem.setEnabled(mClient.isConnected());
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        getActivity().invalidateOptionsMenu();
+        mClient.connect();
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        mClient.disconnect();
     }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-#Locatr
+# Locatr
 
 Locatr queries Flickr for photos near your location. This app covers chapters 33-34 in The BigNerdRanch Guide:Android Programming 3rd Edition.
